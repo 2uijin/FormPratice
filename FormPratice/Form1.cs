60b9819bb8ff85f08c1eda4e4b45bb07dadcfcf5
2uijin <49094082+2uijin@users.noreply.github.com>
2020-06-30 14:56:20 +0900

    초기 커밋.

---
+++ FormPratice/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormPratice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 새파일ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("새파일 두둥등장");
+        }
+
+        private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); //종료
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 배고파ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "배고파배고프다굿!";
+            toolStripProgressBar1.Value = 10;
+        }
+
+        private void 음먹으면들어가ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "그냥그렇누";
+            toolStripProgressBar1.Value = 50;
+        }
+
+        private void 배불러ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "배배배배배불불불불러러러러러";
+            toolStripProgressBar1.Value = 100;
+        }
+    }
+}
